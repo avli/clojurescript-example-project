d9cac173c75418710a7ce51461edca5577037e5b
Andrey Lisin <andrey.lisin@gmail.com>
2019-09-08 23:50:58 +0300

    Update project dependencies

--- project.clj
+++ project.clj
@@ -3,15 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.908"]
-                 [com.cemerick/piggieback "0.2.2"]
-                 [org.clojure/tools.nrepl "0.2.10"]]
-                 
-                 :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/clojurescript "1.10.520"]
+                 [cider/piggieback "0.4.1"]]
+
+                 :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}
 
                  :plugins [[lein-cljsbuild "1.1.7"]
-                           [cider/cider-nrepl "0.15.0-SNAPSHOT"]]
+                           [cider/cider-nrepl "0.22.1"]]
 
                  :cljsbuild {
                   :builds [{:id "dev"
